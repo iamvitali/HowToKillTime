b8e4dbdf95ba01128bd315df96ff262ab03badea
Vitali Bokov <iam@vitali.ee>
2016-07-29 00:23:19 +0100

    + check if any films were found & + messages in case not

--- resources/views/show/results.blade.php
+++ resources/views/show/results.blade.php
@@ -1,15 +1,37 @@
-<div class="panel panel-default">
-	<table class="table table-striped">
-		@foreach (Request::input('Search') as $film)
-			<tr>
-				<td>
-					Title: {{ $film['Title'] }}
-					<br>
-					Year: {{ $film['Year'] }}
-					<br>
-					Type: {{ $film['Type'] }}
-				</td>
-			</tr>
-		@endforeach
-	</table>
-</div>
+@if (Request::input('Response') === 'True')
+	<div class="panel panel-default">
+		<table class="table table-striped">
+			@foreach (Request::input('Search') as $film)
+				<tr>
+					<td>
+						Title: {{ $film['Title'] }}
+						<br>
+						Year: {{ $film['Year'] }}
+						<br>
+						Type: {{ $film['Type'] }}
+					</td>
+				</tr>
+			@endforeach
+		</table>
+	</div>
+@elseif (Request::input('Response') === 'False' && Request::input('Error') === 'Movie not found!')
+	<div class="alert alert-info text-center" role="alert">
+		Nothing found.
+		<br>
+		<br>
+		Try looking for "Batman"!
+	</div>
+@elseif (Request::input('Response') === 'False' && Request::input('Error') === 'Must provide more than one character.')
+	<div class="alert alert-warning text-center" role="alert">
+		Oh, there are so many films that start with that letter! Maybe add one more?
+	</div>
+@elseif (Request::input('Response') === 'False')
+	<div class="alert alert-warning text-center" role="alert">
+		{{-- Displaying the error that was returned by OMDb API: --}}
+		{{ Request::input('Error') }}
+	</div>
+@else
+	<div class="alert alert-danger text-center" role="alert">
+		Wow this is embarrassing but our servers went bananas. Maybe try again? Please?
+	</div>
+@endif
