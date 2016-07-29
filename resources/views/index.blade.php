7e505771cd9340bfbe3f9f32ab6cd2a035203108
Vitali Bokov <iam@vitali.ee>
2016-07-29 23:03:09 +0100

    + waiting 300ms before sending the ajax request
    To prevent spamming requests while user is typing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,6 +32,7 @@
     @parent
 
     $('#iNameOrCode').on('input', function () {
+        clearTimeout(window.timeout_to_start_search);
         findFilmsAndShows(1); // open first page if there are results
     });
 
@@ -45,46 +46,49 @@
 
             var params_in_url_format = '?' + $.param(params);
 
-            window.film_search_ajax = $.ajax({
-                type: 'GET',
-                url: 'http://www.omdbapi.com/' + params_in_url_format,
-                dataType: 'json',
-                beforeSend: function () {
-                    /* show loading icon */
-                    $('#results').html('<div class="text-center"><span class="fa fa-circle-o-notch fa-spin fa-2x"></span></div>');
+            /* Don't send ajax immediately to prevent spamming requests while user is typing */
+            window.timeout_to_start_search = setTimeout(function() {
+                window.film_search_ajax = $.ajax({
+                    type: 'GET',
+                    url: 'http://www.omdbapi.com/' + params_in_url_format,
+                    dataType: 'json',
+                    beforeSend: function () {
+                        /* show loading icon */
+                        $('#results').html('<div class="text-center"><span class="fa fa-circle-o-notch fa-spin fa-2x"></span></div>');
 
-                    /* Check if the previous request is still running and if so cancel it */
-                    if(window.film_search_ajax) window.film_search_ajax.abort();
-                },
-                success: function (result) {
-                    $('#results').load('/showResults/', result, function () {
-                        var items_per_page = 10; // this is set by OMDB API and is currently 10
+                        /* Check if the previous request is still running and if so cancel it */
+                        if(window.film_search_ajax) window.film_search_ajax.abort();
+                    },
+                    success: function (result) {
+                        $('#results').load('/showResults/', result, function () {
+                            var items_per_page = 10; // this is set by OMDB API and is currently 10
 
-                        /* check if there was anything found and there is more than 1 page */
-                        if(result.Response === 'True' && result.totalResults > items_per_page) {
-                            $('.pagination').twbsPagination({
-                                first: false, // hide "First" navigation button
-                                last: false, // hide "Last" navigation button
-                                startPage: page, // current page
-                                totalPages: Math.ceil(result.totalResults / items_per_page), // rounding up so 1.05 pages would become 2 pages
-                                visiblePages: 6,
-                                onPageClick: function (event, pageToSwitchTo) {
-                                    /* only call AJAX if we are trying to switch to a different page */
-                                    if(page !== pageToSwitchTo) {
-                                        findFilmsAndShows(pageToSwitchTo);
+                            /* check if there was anything found and there is more than 1 page */
+                            if(result.Response === 'True' && result.totalResults > items_per_page) {
+                                $('.pagination').twbsPagination({
+                                    first: false, // hide "First" navigation button
+                                    last: false, // hide "Last" navigation button
+                                    startPage: page, // current page
+                                    totalPages: Math.ceil(result.totalResults / items_per_page), // rounding up so 1.05 pages would become 2 pages
+                                    visiblePages: 6,
+                                    onPageClick: function (event, pageToSwitchTo) {
+                                        /* only call AJAX if we are trying to switch to a different page */
+                                        if(page !== pageToSwitchTo) {
+                                            findFilmsAndShows(pageToSwitchTo);
+                                        }
                                     }
-                                }
-                            });
+                                });
+                            }
+                        });
+                    },
+                    error: function (textStatus, errorThrown) {
+                        if(errorThrown !== 'abort') {
+                            var error_message = 'We are very sorry but our online pigeon didn\'t make it back with the data.<br><br>If this happens again please contact admin@vitali.london with the following error code: ' + textStatus.status;
+                            $('#results').html('<div class="alert alert-danger text-center" role="alert">' + error_message + '</div>');
                         }
-                    });
-                },
-                error: function (textStatus, errorThrown) {
-                    if(errorThrown !== 'abort') {
-                        var error_message = 'We are very sorry but our online pigeon didn\'t make it back with the data.<br><br>If this happens again please contact admin@vitali.london with the following error code: ' + textStatus.status;
-                        $('#results').html('<div class="alert alert-danger text-center" role="alert">' + error_message + '</div>');
                     }
-                }
-            });
+                });
+            }, 300);
         } else {
             /* Cancel ajax request if it's still processing */
             window.film_search_ajax.abort();
