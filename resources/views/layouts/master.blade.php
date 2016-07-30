34d2ac28b491eb6502247981fcbf298bd1033558
Vitali Bokov <iam@vitali.ee>
2016-07-30 21:44:02 +0100

    + fix & support for phones to view it in appropriate size

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,9 @@
     <head>
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        /* Is needed for phones to scale it appropriately, otherwise it will be zoomed out */
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
         <title>@yield('title')</title>
 
         <link href="/css/app.css" rel="stylesheet">
